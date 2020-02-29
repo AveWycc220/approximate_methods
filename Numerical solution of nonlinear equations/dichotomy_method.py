c2fc9b747e28614cf523aa2f6da462c4ce1d7592
AveWycc220 <mik16748@gmail.com>
2020-02-29 18:08:21 +0300

    Add check for value

--- Numerical solution of nonlinear equations/dichotomy_method.py
+++ Numerical solution of nonlinear equations/dichotomy_method.py
@@ -1,6 +1,7 @@
 import math
 
 class DichotonomyMethod:
+    # Method that return the given equation.
     def __function(self, x): 
         return x * 10 - 4
     def get_result(self, a, b, e):
@@ -14,11 +15,28 @@ class DichotonomyMethod:
                 a = c
         return c
 
+def is_digit(str):
+    if str.isdigit():
+       return True
+    else:
+        try:
+            float(str)
+            return True
+        except ValueError:
+            return False   
+
 if __name__ == "__main__":
-        print("Input the borders of section")
-        a = int(input())
-        b = int(input())
-        print("Input accuracy")
-        e = float(input())
-        DM = DichotonomyMethod()
-        print(DM.get_result(a,b,e))
+    print("Input the borders of section")
+    a = input()
+    b = input()
+    print("Input accuracy")
+    e = input()
+    if (a.isdigit() and b.isdigit() and is_digit(e)):
+        a = int(a)
+        b = int(b)
+        e = float(e)
+    else:
+        print("Wrong input. Try again..")
+        raise SystemExit('ValueError')
+    DM = DichotonomyMethod()
+    print(DM.get_result(a,b,e))
