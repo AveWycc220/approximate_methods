0ce77bb017eef786e2daf488d0069aae44ab551a
AveWycc220 <mik16748@gmail.com>
2020-03-14 00:26:02 +0300

    Change main.py. Add the ability to change values while operation

--- Numerical solution of nonlinear equations/main.py
+++ Numerical solution of nonlinear equations/main.py
@@ -13,15 +13,27 @@ def is_digit(str):
             return True
         except ValueError:
             return False   
-            
-print("Input the borders of section")
-a = input(); b = input()
-print("Input accuracy")
-e = input()
-if (is_digit(a) and is_digit(b) and is_digit(e)):
-    a = float(a); b = float(b); e = float(e)
-else:
-    print("Wrong input. Try again..")
-    raise SystemExit('ValueError')
-NM = nm.NewtonMethod(); CM = cm.ChordMethod(); DM = dm.DichotonomyMethod()
-print(F'NewtonMethod = {round(NM.get_result(a,b,e),10)} | DichotonomyMethod = {DM.get_result(a,b,e)} | ChordMethod = {round(CM.get_result(a,b,e),10)}')
+
+while True:
+    print("1 -> Input or change borders, 2 -> Output result 0 -> Exit")
+    choose = input()
+    if(choose == "1"):
+        print("Input the borders of section")
+        a = input(); b = input()
+        print("Input accuracy")
+        e = input()
+        if (is_digit(a) and is_digit(b) and is_digit(e)):
+            a = float(a); b = float(b); e = float(e)
+        else:
+            print("Wrong input. Try again..")
+            raise SystemExit('ValueError')
+    elif(choose == "2" and 'a' in locals()):
+        NM = nm.NewtonMethod(); CM = cm.ChordMethod(); DM = dm.DichotonomyMethod()
+        print(F'NewtonMethod = {round(NM.get_result(a,b,e),10)} | DichotonomyMethod = {DM.get_result(a,b,e)} | ChordMethod = {round(CM.get_result(a,b,e),10)}')
+    elif(choose == "0"):
+        break
+    else:
+        if('a' not in locals()):
+            print("Use 1 to input borders before output results")
+        else:
+            print("Wrong number")
