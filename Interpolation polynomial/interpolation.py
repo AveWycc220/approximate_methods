0a365fd4a0ca1d858dc93995db1f6cedbb81ede0
AveWycc220 <mik16748@gmail.com>
2020-04-12 18:46:36 +0300

    Add interpolation

--- Interpolation polynomial/interpolation.py
+++ Interpolation polynomial/interpolation.py
@@ -2,31 +2,64 @@ import math
 import numpy as np
 import matplotlib.pyplot as pl
 
-MAX_POINTS_X_LAGRANGE = 1000
-MAX_POINTS_X_NEWTON = 1000
+step_lagrange = 0.7
+newton_step = 0.7
 
 
-def f(x):
-    return 2 * math.sin(x)
-def lagranz(x,y,t):
+def f(xf):
+    if xf > math.pi:
+        return math.cos(xf)
+    else: 
+        return math.tan(xf)
+
+def lagrange(xl,yl,t):
     polynom=0
-    for j in range(len(y)):
+    for j in range(len(yl)):
         numerator=1; denumerator=1
-        for i in range(len(x)):
+        for i in range(len(xl)):
             if i==j:
                 pass   
             else: 
-                numerator=numerator*(t-x[i])
-                denumerator=denumerator*(x[j]-x[i])
-        polynom=polynom+y[j]*numerator/denumerator
+                numerator=numerator*(t-xl[i])
+                denumerator=denumerator*(xl[j]-xl[i])
+        polynom=polynom+yl[j]*numerator/denumerator
     return polynom
 
+def delta(y,k,i):
+    c=1.0
+    s=0.0
+    for j in range(k):
+        s=s+c*y[i+k-j]
+        c=c*(-1)*(k-j)/(j+1)
+    return s
+
+def newton(x,y):
+    z = 0
+    s_list = list()
+    h = int(round(10/newton_step))
+    for j in np.arange(0,10,newton_step):
+        s = y[0]
+        a=1
+        for i in range(1,h):
+            a *= (z-x[i-1])/(i*newton_step)
+            s += delta(y,i,0)*a
+        z += newton_step
+        s_list.append(s)
+    return s_list
+
 if __name__ == '__main__':
-    x = np.linspace(0,2*math.pi,50, dtype = float)
+    x = np.array([x for x in np.arange(0,10,0.5)], dtype = float)
+    x_list = [x for x in np.arange(0,10,newton_step)]
     y = np.array([f(x[i]) for i in range(len(x))], dtype=float)
-    xnew=np.linspace(np.min(x),np.max(x),MAX_POINTS_X_LAGRANGE)
-    ynew=[lagranz(x,y,i) for i in xnew]
-    pl.plot(xnew,ynew, color = 'red')
-    pl.plot(x,y, color = 'blue')
+    y_list = [f(x_list[i]) for i in range(len(x_list))]
+    xnew=np.array([x for x in np.arange(0,10,step_lagrange)],dtype = float)
+    ylagrange=[lagrange(x,y,i) for i in xnew]
+    ynewton = newton(x_list,y_list)
+    pl.axis(xlim=(0, 10), ylim=(-100, 100))
+    pl.plot(xnew,ylagrange, color = 'red', label = 'lagrange')
+    pl.plot(x,y, color = 'blue', label = '2 * sin(x)', linestyle='dashed')
+    pl.plot(x_list,ynewton, color = 'green', label = 'newton')
     pl.grid()
+    pl.xlim(0,10)
+    pl.ylim(-50,50)
     pl.show()
