42f590381a98bacaba69cd1ce86e2933a75a5e75
AveWycc220 <mik16748@gmail.com>
2020-06-05 15:32:59 +0300

    feat (ode_eulers_method): add implementation on С++

---
+++ Second Order ODE Euler's Method/ode_eulers_method.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+double f(double y){
+    return 4*y;
+}
+
+double check(double x){
+    return exp(2*x);
+}
+
+int main(){
+    double a;
+    double b;
+    double h;
+    int n;
+    a = 0; b = 2;
+    h = 0.001;
+    n = ceil((((b-a)/h + 1)));
+    double x[n];
+    double y[n];
+    double u[n];
+    y[0] = 1;
+    u[0] = 2;
+    for (int i = 0; i<n-1; i++){
+        x[i+1] = x[i] + h;
+        y[i+1] = y[i] + h*u[i];
+        u[i+1] = u[i] + h*f(y[i]);
+        printf("%f | %f | %f\n", x[i+1], y[i+1], check(x[i+1]));
+    }
+    _getch();
+    return 0;
+}
